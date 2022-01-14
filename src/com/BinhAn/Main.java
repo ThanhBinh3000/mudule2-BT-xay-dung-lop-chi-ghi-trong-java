c82373cbb2854096ae77627663f697986be569fc
unknown <binhk4a@gmail.com>
2022-01-14 15:19:25 +0700

    bt

---
+++ src/com/BinhAn/Main.java
@@ -0,0 +1,10 @@
+package com.BinhAn;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("a");
+        student.setClasses("b");
+    }
+}
---
+++ src/com/BinhAn/Student.java
@@ -0,0 +1,18 @@
+package com.BinhAn;
+
+public class Student {
+    private String name;
+    private String classes;
+    Student(){
+        this.name="Bình An";
+        this.classes="C1121G1";
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+}
